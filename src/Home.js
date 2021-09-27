426a0780ef9c8bdee5b05b6da1d8e3c1d403c0f3
asheikh <asim.sardar.sheikh@gmail.com>
2021-09-27 13:07:54 +0200

    Sending down a callback into child component, functions as props

--- src/Home.js
+++ src/Home.js
@@ -4,14 +4,18 @@ import BlogList from './BlogList'
 const Home = () => {
   const [blogs, setBlogs] = useState([
     {title: 'My new website', body: 'lorem ipsem', author: 'mario', id: 1},
-    {title: 'Welcome Party!', body: 'lorem ipsem', author: 'yoshi', id: 2},
     {title: 'Web dev top tips', body: 'lorem ipsem', author: 'mario', id:3},
+    {title: 'Welcome Party!', body: 'lorem ipsem', author: 'yoshi', id: 2},
   ])
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id)
+    setBlogs(newBlogs)
+  }
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="A Blog"/>
-      <BlogList blogs={blogs.filter(blog => blog.author === 'mario')} title="Mario's Blogs"/>
+      <BlogList blogs={blogs} title="A Blog" handleDelete={handleDelete}/>
     </div>
   )
 }
