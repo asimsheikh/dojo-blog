bf2e86a3d7f6c54322e38f418ab7d321e65cc949
asheikh <asim.sardar.sheikh@gmail.com>
2021-09-27 14:33:54 +0200

    Updating data with conditional template logic

--- src/Home.js
+++ src/Home.js
@@ -2,26 +2,29 @@ import { useState, useEffect } from 'react'
 import BlogList from './BlogList'
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {title: 'My new website', body: 'lorem ipsem', author: 'mario', id: 1},
-    {title: 'Web dev top tips', body: 'lorem ipsem', author: 'mario', id:3},
-    {title: 'Welcome Party!', body: 'lorem ipsem', author: 'yoshi', id: 2},
-  ])
-
-  const [name, setName] = useState('mario')
+  const [blogs, setBlogs] = useState(null)
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id)
     setBlogs(newBlogs)
   }
 
-  useEffect(() => console.log('use effect ran: ' + name), [name])
+  useEffect(() => {
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        console.log(data)
+        setBlogs(data)
+      })
+  }, [])
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="A Blog" handleDelete={handleDelete}/>
-      <button onClick={() => setName('luigi')}>change name</button>
-      <p>{name}</p>
+      { blogs && 
+        <BlogList blogs={blogs} title="A Blog" handleDelete={handleDelete}/>
+      }
     </div>
   )
 }
