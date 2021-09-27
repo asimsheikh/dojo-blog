ba597a5e24a4104f1136ada178782cddef912ad9
asheikh <asim.sardar.sheikh@gmail.com>
2021-09-27 12:28:17 +0200

    Using props to extract out Blogs component

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,18 @@
 import { useState } from 'react'
+import BlogList from './BlogList'
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: 'My new website', body: 'lorem ipsem', author: 'mario', id: 1}, 
-        {title: 'Welcome Party!', body: 'lorem ipsem', author: 'yoshi', id: 2},
-        {title: 'Web dev top tips', body: 'lorem ipsem', author: 'mario', id:3},
-    ])
+  const [blogs, setBlogs] = useState([
+    {title: 'My new website', body: 'lorem ipsem', author: 'mario', id: 1},
+    {title: 'Welcome Party!', body: 'lorem ipsem', author: 'yoshi', id: 2},
+    {title: 'Web dev top tips', body: 'lorem ipsem', author: 'mario', id:3},
+  ])
 
-    return (
-        <div className="home">
-            {blogs.map(blog => ( 
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                </div>
-            ))}
-        </div>
-    )
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="A Blog"/>
+    </div>
+  )
 }
 
 export default Home
