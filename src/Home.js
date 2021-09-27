5a957aa47816c684ab4ced8cfcdb7629c076f489
asheikh <asim.sardar.sheikh@gmail.com>
2021-09-27 14:37:51 +0200

    Removed deleteHandler

--- src/Home.js
+++ src/Home.js
@@ -4,11 +4,6 @@ import BlogList from './BlogList'
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id)
-    setBlogs(newBlogs)
-  }
-
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then(res => {
@@ -23,7 +18,7 @@ const Home = () => {
   return (
     <div className="home">
       { blogs && 
-        <BlogList blogs={blogs} title="A Blog" handleDelete={handleDelete}/>
+        <BlogList blogs={blogs} title="A Blog" />
       }
     </div>
   )
