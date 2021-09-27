27fcd954c539c5a3b74502498663579a6f2cb5f0
asheikh <asim.sardar.sheikh@gmail.com>
2021-09-27 12:33:39 +0200

    Reusing components example filters blogs

--- src/Home.js
+++ src/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
   return (
     <div className="home">
       <BlogList blogs={blogs} title="A Blog"/>
+      <BlogList blogs={blogs.filter(blog => blog.author === 'mario')} title="Mario's Blogs"/>
     </div>
   )
 }
