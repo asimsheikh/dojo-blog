1dc8021ac4c31a568e2245eb902145a55090a0dd
asheikh <asim.sardar.sheikh@gmail.com>
2021-09-27 14:43:58 +0200

    Conditional loading using isPending flag

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import BlogList from './BlogList'
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
+  const [isPending, setIsPending] = useState(true)
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
@@ -12,14 +13,14 @@ const Home = () => {
       .then(data => {
         console.log(data)
         setBlogs(data)
+        setIsPending(false)
       })
   }, [])
 
   return (
     <div className="home">
-      { blogs && 
-        <BlogList blogs={blogs} title="A Blog" />
-      }
+      {isPending && <div>Loading...</div>}
+      { blogs && <BlogList blogs={blogs} title="A Blog" /> }
     </div>
   )
 }
